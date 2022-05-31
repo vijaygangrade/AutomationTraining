2c44da23fad31dbb762ceb36a60885c433bde54e
gwl <gwl@DESKTOP-9GGG7IO>
2022-05-31 17:24:05 +0530

    c

--- src/practice/Runner.java
+++ src/practice/Runner.java
@@ -1,33 +1,28 @@
 package practice;
 
-public class Runner 
-{
-	
-	public static void main(String[] args) throws InterruptedException 
-	{
-		 // TODO Auto-generated method stub
+public class Runner {
+
+	public static void main(String[] args) throws InterruptedException {
+		// TODO Auto-generated method stub
 		CustomerLogin cust_login = new CustomerLogin();
-		
+
 		cust_login.openBrowser();
-		
+
 		String user_role = "admin";
-		
-		if (user_role.equals("admin")) 
-		{
+
+		if (user_role.equals("admin")) {
 			cust_login.adminLogin();
 			System.out.println("Admin");
-		}
-		else
-		{
+		} else {
 			System.out.println("Customer");
 			cust_login.customerLogin();
-			
+
 		}
-				
+
 		Thread.sleep(15000);
 		System.out.println("Close");
 		cust_login.closeBrowser();
-		
-		}
+//
+	}
 
 }
